03f59c565c5fe1086f9ff456b9d122e192608e15
Frans Narciso <fransnarc@gmail.com>
2024-11-13 17:01:04 -0600

    Modified so products page on admin dashboard works.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+
+    def self.ransackable_attributes(auth_object = nil)
+        ["brand", "created_at", "id", "ingredients", "price", "product_name", "ranking", "updated_at"]
+    end
 end
