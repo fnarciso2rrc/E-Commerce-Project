6d84a5cea3e55830430d27203d1fe56f30ccd2f6
Frans Narciso <fransnarc@gmail.com>
2024-11-14 14:20:22 -0600

    Added validations and relationship in category model.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+    has_many :product_categories
+    has_many :products, through: :product_categories
+
+    validates :category_type, presence: true
+    validates :category_type, uniqueness: true
 end
