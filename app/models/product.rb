4ffc9a1c0cc0d0ffccc698b7039521db0fadb8ef
Frans Narciso <fransnarc@gmail.com>
2024-11-14 14:16:49 -0600

    Added validations and relationship in product model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+    has_many :product_categories
+    has_many :categories, through: :product_categories
+
+    validates :product_name, :brand, :price, :ingredients, presence: true
+    validates :price, numericality: {greater_than: 0}
 
     def self.ransackable_attributes(auth_object = nil)
         ["brand", "created_at", "id", "ingredients", "price", "product_name", "ranking", "updated_at"]
