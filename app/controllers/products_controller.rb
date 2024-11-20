8a36225a5079886b674e7ed2a9cca74ac503180f
Frans Narciso <fransnarc@gmail.com>
2024-11-20 17:36:43 -0600

    Added a search form.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,20 @@ class ProductsController < ApplicationController
 
   # GET /products or /products.json
   def index
+    @categories = Category.all
+
+    keywords = params[:keywords]
+    category_id = params.dig(:category, :id)
+
     @products = Product.all
+
+    if keywords.present?
+      @products = @products.where("product_name LIKE ?", "%#{keywords}%")
+    end
+
+    if category_id.present?
+      @products = @products.where(category_id: category_id)
+    end
   end
 
   # GET /products/1 or /products/1.json
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,20 @@ class Product < ApplicationRecord
     validates :product_name, :brand, :price, :ingredients, presence: true
     validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 
+    def self.search(keywords, category_id)
+        products = all
+
+        if keywords.present?
+            products = products.where("product_name LIKE ?", "%#{keywords}%")
+        end
+
+        if category_id.present?
+            products = products.where(category_id: category_id)
+        end
+
+        products
+    end 
+
     def self.ransackable_associations(auth_object = nil)
         ["category"]
     end
