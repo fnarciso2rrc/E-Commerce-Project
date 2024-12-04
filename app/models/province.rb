afa7ce6df18e3d898826039a8936dc15a0319961
Frans Narciso <fransnarc@gmail.com>
2024-12-04 17:36:42 -0600

    Added ransackable in province and user model to get province in active admin.

--- app/models/province.rb
+++ app/models/province.rb
@@ -3,4 +3,12 @@ class Province < ApplicationRecord
 
     validates :province_name, presence: true, uniqueness: true
     validates :alpha_code, presence: true, length: { is: 2 }, uniqueness: true
+
+    def self.ransackable_associations(auth_object = nil)
+        ["users"]
+    end
+
+    def self.ransackable_attributes(auth_object = nil)
+        ["alpha_code", "created_at", "id", "id_value", "province_name", "tax", "updated_at"]
+    end
 end
